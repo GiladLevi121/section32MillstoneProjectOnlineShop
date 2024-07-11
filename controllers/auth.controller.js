1b0c7029495edafd29ae9da65c1a78f1ef38ee84
Gilad Levi <giladlevi121@gmail.com>
2024-07-11 08:19:33 +0300

    last updates

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,47 @@
-const User = require('../models/user.model');
-const authUtil = require('../util/authentication');
+const User = require("../models/user.model");
+const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res) {
-  res.render('customer/auth/signup');
+  res.render("customer/auth/signup");
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
+  const enteredDate = {
+    email: req.body.email,
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+
+  if (
+    !validation.userDetaisAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Password must be at least 6 characters long, postal code must be 5 characters long.",
+        ...enteredDate,
+      },
+      function () {
+        res.redirect("/signup");
+      }
+    );
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -15,21 +51,56 @@ async function signup(req, res) {
     req.body.city
   );
 
-  await user.signup();
+  try {
+    const existsAlready = await user.existsAlready();
 
-  res.redirect('/login');
+    if (existsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User exists already! Try logging in instead!",
+          ...enteredDate,
+        },
+        function () {
+          res.redirect("/signup");
+        }
+      );
+      return;
+    }
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect("/login");
 }
 
 function getLogin(req, res) {
-  res.render('customer/auth/login');
+  res.render("customer/auth/login");
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double-check your email and password!",
+    email: user.email,
+    password: user.password,
+  };
 
   if (!existingUser) {
-    res.redirect('/login');
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
@@ -38,18 +109,26 @@ async function login(req, res) {
   );
 
   if (!passwordIsCorrect) {
-    res.redirect('/login');
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
   authUtil.createUserSession(req, existingUser, function () {
-    res.redirect('/');
+    res.redirect("/");
   });
+}
+
+function logout(req, res) {
+  authUtil.destroyUserAuthSession(req);
+  res.redirect("/login");
 }
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
   signup: signup,
-  login: login
+  login: login,
+  logout: logout,
 };
---
+++ util/validation.js
@@ -0,0 +1,28 @@
+function isEmpty(value) {
+  return !value || value.trim() === "";
+}
+
+function userCredentialsAreValid(email, password) {
+  return (
+    email && email.includes("@") && password && password.trim().length >= 6
+  );
+}
+
+function userDetaisAreValid(email, password, name, street, postal, city) {
+  return (
+    userCredentialsAreValid(email, password) &&
+    !isEmpty(name) &&
+    !isEmpty(street) &&
+    !isEmpty(postal) &&
+    !isEmpty(city)
+  );
+}
+
+function emailIsConfirmed(email, confirmedEmail) {
+  return email === confirmedEmail;
+}
+
+module.exports = {
+  userDetaisAreValid: userDetaisAreValid,
+  emailIsConfirmed: emailIsConfirmed,
+};
